3cd6b9458eaf7347afdd8ba62c084b3836b735dd
Tommy Ku <zoo1929@outlook.com>
2018-05-26 23:13:58 +0800

    ADD: polybar on multiple monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar mybar &
+# start polybar on multiple monitors https://github.com/jaagr/polybar/issues/763
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
 
 echo "Bars launched..."
